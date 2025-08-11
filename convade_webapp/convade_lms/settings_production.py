bfab6b6b3490703af891ef5210ebd25ea4a301f0
sparrkz <eminspark@gmail.com>
2025-08-11 07:15:34 +0100

    mehn

--- convade_webapp/convade_lms/settings_production.py
+++ convade_webapp/convade_lms/settings_production.py
@@ -30,7 +30,9 @@ DATABASES = {
 
 # Static files configuration for cPanel
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 
 # Media files configuration
